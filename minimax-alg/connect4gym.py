b58eb8407f9679a13fb5325f1ab4ad6e1b84fc83
7174Andy <hep003@ucsd.edu>
2025-08-13 09:18:06 -0700

    add series of matches

--- minimax-alg/connect4gym.py
+++ minimax-alg/connect4gym.py
@@ -2,14 +2,35 @@ from connect4_gym.env import Connect4Env
 from agents.minimax_agent import MiniMaxAgent
 import numpy as np
 
-env = Connect4Env()
-env.reset()
-ai = MiniMaxAgent(player_id=0, max_depth=6)
-env.render()
-while env.winner is None:
-    if env.current_player == ai.player_id:
-        move = ai.get_action(env)
-    else:
-        move = np.random.choice(env.get_moves())  # or your second agent
-    _, _, done, _ = env.step(move)
-    env.render()
+
+def run_match(player0):
+    env = Connect4Env()
+    env.reset()
+    while env.winner is None:
+        if env.current_player == player0.player_id:
+            move = player0.get_action(env)
+        else:
+            move = np.random.choice(env.get_moves())
+        env.step(move)
+        env.render()
+
+    return env.winner
+
+
+def main():
+    wins = draws = losses = 0
+    for i in range(100):
+        player1 = MiniMaxAgent(player_id=1, max_depth=6)
+        winner = run_match(player1)
+        if winner == 1:
+            wins += 1
+        elif winner == 0:
+            losses += 1
+        else:
+            draws += 1
+
+    print(f"AI - Wins: {wins}, Losses: {losses}, Draws: {draws}")
+
+
+if __name__ == "__main__":
+    main()
